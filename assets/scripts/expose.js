140a2a20dadecd61a95c8d93d9a3be51829b0860
ellatung1 <114188007+ellatung1@users.noreply.github.com>
2022-10-28 10:18:21 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,16 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  
+  let airHorn = document.getElementById("horn-select").selectedOptions[1].value;
+  let carHorn = document.getElementById("horn-select").selectedOptions[2].value;
+  let partyHorn = document.getElementById("horn-select").selectedOptions[3].value;
+  
+  airHorn.addEventListener(‘click’, changePicture() {
+
+     document.getElementById("img").src=a;
+     document.querySelector('img[src=""]');
+  
+  })
+  
 }
