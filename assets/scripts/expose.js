a78ecc0bdd450dc162bc267bbcd57e78b8754bff
ellatung1 <114188007+ellatung1@users.noreply.github.com>
2022-10-28 11:05:22 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -7,23 +7,27 @@ function init() {
   let airHorn = document.getElementById("horn-select").selectedOptions[1].value;
   let carHorn = document.getElementById("horn-select").selectedOptions[2].value;
   let partyHorn = document.getElementById("horn-select").selectedOptions[3].value;
+  let audio = document.getElementsByClassName("Hidden");
   
 }
   
   airHorn.addEventListener(‘click’, changePicture() {
 
-     document.querySelector('img[alt="No image selected"]') = img.src.replace("assets/images/air-horn.svg")
+     document.querySelector('img[alt="No image selected"]') = img.src.replace("assets/images/air-horn.svg");
+     audio.src.replace("assets/audio/air-horn.mp3);
   
   })
   
   carHorn.addEventListener(‘click’, changePicture() {
 
-     document.querySelector('img[alt="No image selected"]') = img.src.replace("assets/images/car-horn.svg")
+     document.querySelector('img[alt="No image selected"]') = img.src.replace("assets/images/car-horn.svg");
+     audio.src.replace("assets/audio/car-horn.mp3);
   
   })
   
   partyHorn.addEventListener(‘click’, changePicture() {
 
-     document.querySelector('img[alt="No image selected"]') = img.src.replace("assets/images/party-horn.svg")
+     document.querySelector('img[alt="No image selected"]') = img.src.replace("assets/images/party-horn.svg");
+     audio.src.replace("assets/audio/party-horn.mp3);
   
   })
